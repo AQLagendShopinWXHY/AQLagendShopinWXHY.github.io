f33326c26ff145a8168ea4490159cf42cc11507b
AQLagendShopinWXHY <2211546588@qq.com>
2025-10-04 21:18:27 +0800

    enter

--- js/mo_table.js
+++ js/mo_table.js
@@ -1,13 +1,65 @@
 const base = window.location.origin;
+const checkbox = document.getElementById('checkbox_pd');
+const downloadButton = document.getElementById("dowload_check");
+let timeout_tips;
+let links;
+
+    document.getElementById("checkbox_pd").addEventListener("change", function() {
+        const checkbox = document.querySelector("input[type='checkbox']");
+        if (checkbox.checked) {
+            downloadButton.disabled = false;
+            downloadButton.style.backgroundColor = "#04AA6D"; // 绿色
+            downloadButton.textContent = "✓确认"
+        } else {
+            downloadButton.disabled = true;
+            downloadButton.style.backgroundColor = "rgba(136, 136, 136, 0.2)"; // 灰色
+            downloadButton.textContent = "不给你点！"
+        }
+    });
 
 
-    // 获取触发模态框的自定义属性
+
+
+    document.getElementById("dowload_check").addEventListener("click", function() {
+        const checkbox = document.querySelector("input[type='checkbox']");
+        if (checkbox.checked) {
+            window.open(links, "_blank");
+        } else {
+            clearTimeout(timeout_tips);
+            var checkfalseModal = document.getElementById("myModal_checkfalse");
+            checkfalseModal.style.display = "block";
+            checkfalseModal.querySelector(".modal-content").classList.remove("modal-hide");
+
+            function close_function(){
+                checkfalseModal.querySelector(".modal-content").classList.add("modal-hide");
+                checkfalseModal.querySelector(".modal-content").addEventListener("animationend", function () {
+                checkfalseModal.style.display = "none";
+                }, { once: true });
+            }
+
+            timeout_tips = setTimeout(function() {
+                close_function()
+                checkfalseModal.removeEventListener("click",remove_setTimeOut);
+            }, 3000);
+
+            function remove_setTimeOut(e){
+                if(e.target === checkfalseModal || e.target.closest("#myModal_checkfalse")){
+                    clearTimeout(timeout_tips);
+                    //还是确保一下吧
+                    close_function();
+                    checkfalseModal.removeEventListener("click",remove_setTimeOut);
+                }
+            }
+
+            checkfalseModal.addEventListener("click",remove_setTimeOut);
+        }
+    });
+
+
         var modalButtons = document.querySelectorAll("[data-modal-action]");
 
-        // 获取模态框主体
         var modals = document.querySelectorAll(".modal");
 
-        // 监听模态框的按钮事件
         modalButtons.forEach(function(button) {
             button.addEventListener("click", function() {
                 var targetId = this.dataset.modalTarget;
@@ -15,7 +67,7 @@ const base = window.location.origin;
 
                 if (targetModal) {
                     var action = this.dataset.modalAction;
-                    var modalData = this.dataset.modalData; // 获取自定义数据属性
+                    var modalData = this.dataset.modalData;
 
                     if (action === "open") {
                         openModal(targetModal,modalData);
@@ -30,38 +82,73 @@ const base = window.location.origin;
         function openModal(modal,modalData) {
             modal.style.display = "block";
             modal.querySelector(".modal-content").classList.remove("modal-hide");
-
             (async () => {
-            const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
-            const tips = check_Tips(tip,modalData)
-            const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
-            const updatetime = check_Times(timelist,modalData)
+
+                if (typeof modalData === "string") {
+                    try {
+                        modalData = JSON.parse(modalData);
+                    } catch (e) {
+                        modalData = modalData;
+                    }
+                } else {
+                    modalData = modalData;
+                }
 
                 if(modalData==="in_GetCard_Buy"){
-                    //修改模态框中的相关内容
-                    const json_data = await fetchData("./viewpoint/json/CARD_Shouhou_GETCard_BUY.json");
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
+                    const tips = check_Tips(tip,modalData)
+                    const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
+                    const updatetime = check_Times(timelist,modalData)
+                    const json_data = await fetchData("/viewpoint/json/CARD_Shouhou_GETCard_BUY.json");
                     var title = "卡密购买"
                     updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
                 }else if(modalData==="in_GetCard_Get"){
-                    const json_data = await fetchData("./viewpoint/json/CARD_Shouhou_GETCard_URL.json");
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
+                    const tips = check_Tips(tip,modalData)
+                    const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
+                    const updatetime = check_Times(timelist,modalData)
+                    const json_data = await fetchData("/viewpoint/json/CARD_Shouhou_GETCard_URL.json");
                     var title = "卡密提取"
                     updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
                 }else if(modalData==="in_AfterCard_QQgroup"){
-                    const json_data = await fetchData("./viewpoint/json/CARD_Shouhou_AfterCard_QQGroup.json");
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
+                    const tips = check_Tips(tip,modalData)
+                    const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
+                    const updatetime = check_Times(timelist,modalData)
+                    const json_data = await fetchData("/viewpoint/json/CARD_Shouhou_AfterCard_QQGroup.json");
                     var title = "加入售后群"
                     updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
                 }else if(modalData==="in_Download"){
-                    const json_data = await fetchData("./viewpoint/json/CARD_Shouhou_StartDownload_URL.json");
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
+                    const tips = check_Tips(tip,modalData)
+                    const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
+                    const updatetime = check_Times(timelist,modalData)
+                    const json_data = await fetchData("/viewpoint/json/CARD_Shouhou_StartDownload_URL.json");
                     var title = "辅助下载"
-                    updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
+                    updateModalContent_download(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
+                    // updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
                 }else if(modalData==="in_FansClub"){
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
+                    const tips = check_Tips(tip,modalData)
+                    const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
+                    const updatetime = check_Times(timelist,modalData)
                     const json_data = await fetchData("/viewpoint/json/CARD_Shouhou_FansClub.json");
                     var title = "加入辉夜粉丝群（非售后群）"
                     updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
                 }else if(modalData==="in_WechatLinks"){
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips.json");
+                    const tips = check_Tips(tip,modalData)
+                    const timelist = await fetchData("/viewpoint/json/CARD_Shouhou_Latest_time.json")
+                    const updatetime = check_Times(timelist,modalData)
                     const json_data = await fetchData("/viewpoint/json/CARD_Shouhou_WechatLinks.json");
                     var title = "关注辉夜公众号"
                     updateModalContent(modal, title, getCarddatahandle(modal, json_data),tips,updatetime)
+                }else if(Array.isArray(modalData)){
+                    links ="";
+                    const tip = await fetchData("/viewpoint/json/CARD_Shouhou_Tips_downloadtrue.json");
+                    const tips = check_Tips(tip,modalData[0])
+                    links = modalData[3];
+                    updateModalContent_DownloadTrue(modal, getCarddatahandle_DownloadTrue(modalData[0],modalData[1],modalData[2]),tips)
                 }
             })();
         }
@@ -71,6 +158,12 @@ const base = window.location.origin;
             modal.querySelector(".modal-content").classList.add("modal-hide");
             modal.querySelector(".modal-content").addEventListener("animationend", function () {
                 modal.style.display = "none";
+                if (checkbox && checkbox.checked !== false) {
+                                checkbox.checked = false;
+                                downloadButton.disabled = true;
+                                downloadButton.style.backgroundColor = "rgba(136, 136, 136, 0.2)"; // 灰色
+                                downloadButton.textContent = "✓确认"
+                            }
             }, { once: true });
         }
 
@@ -103,10 +196,19 @@ const base = window.location.origin;
             return null
         }
 
+        function getCarddatahandle_DownloadTrue(modal,taobaolink,weidianlink){
+                content = '<a href='+taobaolink+' target="_blank" class="shop-link">'+
+                    '<img src="'+modal+'_card_tb.png"/>'+
+                    '<div class="shop-text taobao-text">淘宝</div>'+
+                    '</a>'+
+                    '<a href='+weidianlink+' target="_blank" class="shop-link">'+
+                    '<img src="'+modal+'_card_wd.png"/>'+
+                    '<div class="shop-text weidian-text">微店</div>'+
+                    '</a>'
+                return content
+        }
+
         function getCarddatahandle(modal, data){
-            //测试获取到的标签id和异步获取到的json
-            //获取完整的标识符key
-            //标记组成tr的部分
             var content = "";
             var data_key = Object.keys(data[0]);
             var data_keyFirst = Object.keys(data[0])[0];
@@ -150,9 +252,56 @@ const base = window.location.origin;
                 }
 
             
-                if(data[i][part][k]===null || part==="button_color"){
+                if(data[i][part][k]===null &&part!="use_card" || part==="button_color"|| part==="use_card_taobao"|| part==="use_card_weidian"|| part==="use_card_URL"){
                     continue;
                 }
+
+                if(part==="use_card"){
+                    if(data[i][part][k]=="My"){
+                        const linkdata = [];
+                            linkdata.push(data[i][data_key[j]][k])
+                            linkdata.push(data[i][data_key[j+1]][k])
+                            linkdata.push(data[i][data_key[j+2]][k])
+                            linkdata.push(data[i][data_key[j+3]][k])
+
+                        content +="<td style='border:1px solid #000;width:30%'>" +
+                                            "<button class='button' style='background-color:" + data[i][data_key[j+4]][k] + ";' data-modal-action='open' data-modal-target='myModal_2' data-modal-data="+JSON.stringify(linkdata)+">点击跳转</button>" +
+                                            "</td>";
+                                            continue;
+                    }else if(data[i][part]=="mini"){
+                        const linkdata = [
+                            data[i][part],
+                            data[i][data_key[j+1]],
+                            data[i][data_key[j+2]],
+                            data[i][data_key[j+3]]
+                        ];
+                        //迷你只有一个版本所以暂时不需要定位第几个
+                        content +="<td style='border:1px solid #000;width:30%'>" +
+                                            "<button class='button' style='background-color:" + data[i][data_key[j+4]] + ";' data-modal-action='open' data-modal-target='myModal_2' data-modal-data="+JSON.stringify(linkdata)+">点击跳转</button>" +
+                                            "</td>";
+                                            continue;
+                    }else if(data[i][part][k]=="keqi"){
+                        const linkdata = [
+                            data[i][part][k],
+                            data[i][data_key[j+1]][k],
+                            data[i][data_key[j+2]][k],
+                            data[i][data_key[j+3]][k]
+                        ];
+                        content +="<td style='border:1px solid #000;width:30%'>" +
+                                            "<button class='button' style='background-color:" + data[i][data_key[j+4]][k]  + ";' data-modal-action='open' data-modal-target='myModal_2' data-modal-data="+JSON.stringify(linkdata)+">点击跳转</button>" +
+                                            "</td>";
+                                            continue;
+                    }else{
+                        //没做对只有一行的优化，后续做
+                        content +="<td style='border:1px solid #000;width:30%'>" +
+                                            "<a href='" + data[i][data_key[j+3]][k] + "' target='_blank'>" +
+                                            "<button class='button' style='background-color:" + data[i][data_key[j+4]][k] + ";'>点击跳转</button>" +
+                                            "</a></td>";
+                                            continue;
+                    }
+                }
+
+
                 if(part==="URL"){
                     if(arrLength != 1){
                     content += "<td rowspan='" + pd_null(data[i][part],k) + "' style='border:1px solid #000;width:30%'>" +
@@ -184,45 +333,12 @@ const base = window.location.origin;
 
         }
 
-    //      function getCarddatahandle(modal, data){
-    //         console.log(modal,data);
-    //         //测试获取到的标签id和异步获取到的json
-    //         //获取完整的标识符key
-    //         //标记组成tr的部分
-    //         var content = "";
-    //         var data_key = Object.keys(data[0]);
-    //         var data_keyFirst = Object.keys(data[0])[0];
-            
-    // for (var i = 0; i < data.length; i++) {
-    //     // 第一列rowspan
-    //     var arrLength = data[i][data_key[1]].length;
-    //     for (var k = 0; k < arrLength; k++) {
-    //         content += "<tr>";
-    //         if (k === 0) {
-    //             content += "<td rowspan='" + arrLength + "' style='border:1px solid #000;'>" + data[i][data_keyFirst] + "</td>";
-    //         }
-    //         // 其余字段同一行显示同一索引内容
-    //         for (var j = 1; j < data_key.length; j++) {
-    //             var part = data_key[j];
-    //             console.log(data[i][part][k])
-    //             if(data[i][part][k]===null){
-    //                 continue;
-    //             }
-    //             content += "<td rowspan='" + pd_null(data[i][part],k) + "'style='border:1px solid #000;'>" + data[i][part][k] + "</td>";
-    //         }
-    //         content += "</tr>";
-    //     }
-    // }
-    // return content;
-
-    //     }
-
         function pd_null(data, inround) {
             let count = 0;
             for (let i = inround + 1; i < data.length && data[i] === null; i++) {
                 count++;
             }
-            return count + 1; // 最终返回时多算1
+            return count + 1; 
         }
 
         //修改模态框中的相关内容
@@ -237,7 +353,62 @@ const base = window.location.origin;
             }
             if (contentElement) {
                 contentElement.innerHTML = content;
-                // contentElement.textContent = content;
+            }
+            if (tipsElement) {
+                tipsElement.textContent = "Tips:"+tips;
+            }
+            if (updateTimeElement) {
+                updateTimeElement.textContent = "更新时间："+updatetime;
+            }
+        }
+
+        function updateModalContent_DownloadTrue(modal,content,tips) {
+            var contentElement = modal.querySelector("span");
+            var tipsElement = modal.querySelector("#tips");
+            if (contentElement) {
+                contentElement.innerHTML = content;
+            }
+            if (tipsElement) {
+                tipsElement.textContent = "Tips:"+tips;
+            }
+        }
+        
+
+
+        function updateModalContent_download(modal, title, content,tips,updatetime) {
+            var titleElement = modal.querySelector("h3");
+            var contentElement = modal.querySelector("table");
+            var tipsElement = modal.querySelector("#tips");
+            var updateTimeElement = modal.querySelector("#update-time");
+
+            if (titleElement) {
+                titleElement.textContent = title;
+            }
+            if (contentElement) {
+                contentElement.innerHTML = content;
+                contentElement.addEventListener("click", function (e) {
+                    const button = e.target.closest("[data-modal-action='open']");
+                        var targetId = button.dataset.modalTarget;
+                        var targetModal = document.getElementById(targetId);
+                        if (targetModal) {
+                        var action = button.dataset.modalAction;
+                        var modalData = button.dataset.modalData; 
+
+                        if (action === "open") {
+                            openModal(targetModal,modalData);
+                        } else if (action === "close") {
+                            closeModal(targetModal);
+                        }
+                    }
+
+                    window.onclick = function (event) {
+                        modals.forEach(function(modal) {
+                            if (event.target === modal) {
+                                closeModal(modal);
+                            }
+                        });
+                    };
+                });
             }
             if (tipsElement) {
                 tipsElement.textContent = "Tips:"+tips;
@@ -248,6 +419,8 @@ const base = window.location.origin;
                 // contentElement.textContent = content;
             }
         }
+
+
         // 监听模态框的关闭时事件
         modals.forEach(function(modal) {
             var closeButton = modal.querySelector(".close");
